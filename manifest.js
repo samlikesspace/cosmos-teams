b7ef10b3cab21313cd304bbcc9adb7ef04a96259
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-04-05 23:17:20 +1000

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,26 @@
+{
+    "format_version": 2,
+    "header": {
+        "name": "Staff & Chat Ranks",
+        "description": "Moderation tools with . commands",
+        "uuid": "56d78a12-e34b-4c12-a891-fb8231c54321",
+        "version": [1, 0, 0],
+        "min_engine_version": [1, 20, 0]
+    },
+    "modules": [
+        {
+            "description": "Script module",
+            "type": "script",
+            "language": "javascript",
+            "uuid": "a1234567-b890-c123-d456-e7890f123456",
+            "version": [1, 0, 0],
+            "entry": "scripts/main.js"
+        }
+    ],
+    "dependencies": [
+        {
+            "module_name": "@minecraft/server",
+            "version": "1.10.0"
+        }
+    ]
+}
