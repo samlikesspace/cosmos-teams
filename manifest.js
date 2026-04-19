a51ae5a976d0848ae31c6e4a60cbd051b0ca0ad2
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-19 14:12:24 +1000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@
     "format_version": 2,
     "header": {
         "name": "§sCosmic Teams§r",
-        "description": "§l§sCosmic Teams§r is a mod that lets users make teams.\n\n§oFeatures includes team chat, homes, teleporting, a combat syste  and more!",
+        "description": "§l§sCosmic Teams§r is a mod that lets users make teams.\n\n§oFeatures includes team chat, homes, teleporting, a combat system and more!",
         "uuid": "64a50b0e-52a0-43a5-afcb-5e3aa3e5bdbc",
         "version": [1, 0, 0],
         "min_engine_version": [1, 20, 0]
